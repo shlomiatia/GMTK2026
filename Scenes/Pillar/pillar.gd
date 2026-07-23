8e8f74304a16bb442cba691909f9ed9202fe70d0
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-07-23 21:17:32 +0300

    pillar

---
+++ Scenes/Pillar/pillar.gd
@@ -0,0 +1,24 @@
+@tool
+class_name Pillar extends Node2D
+
+@export var radius: float = 50.0:
+	set(value):
+		radius = value
+		_update()
+
+@onready var _collision_shape: CollisionShape2D = $StaticBody2D/CollisionShape2D
+
+
+func _ready() -> void:
+	_update()
+
+
+func _update() -> void:
+	if !is_node_ready():
+		return
+	(_collision_shape.shape as CircleShape2D).radius = radius
+	queue_redraw()
+
+
+func _draw() -> void:
+	draw_circle(Vector2.ZERO, radius, Color.BLACK)
