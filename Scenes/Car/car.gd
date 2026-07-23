42aa8b8a93c8adb459feaaad46d3587d1ae176b4
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-07-23 11:59:25 +0300

    input efactor

--- Scenes/Car/car.gd
+++ Scenes/Car/car.gd
@@ -12,91 +12,73 @@ var _crank_degrees: float = 0.0
 var _last_full_rotations: int = 0
 var _is_launched: bool = false
 
-var _mouse_cranking: bool = false
-var _mouse_last_angle: float = 0.0
-
-var _pad_cranking: bool = false
-var _pad_last_angle: float = 0.0
-var _pad_trigger_held: bool = false
+var _cranking: bool = false
+var _last_angle: float = NAN
+var _using_mouse: bool = false
 
 
 func _process(_delta: float) -> void:
-    if _is_launched:
-        return
-    _handle_mouse_input()
-    _handle_gamepad_input()
+	if _is_launched:
+		return
+	if Input.is_action_just_pressed("crank"):
+		_using_mouse = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+		_cranking = true
+		_last_angle = _get_crank_angle()
+	elif Input.is_action_just_released("crank"):
+		_cranking = false
+		_launch()
+	elif _cranking:
+		var angle := _get_crank_angle()
+		if is_nan(angle):
+			_last_angle = NAN
+		else:
+			if !is_nan(_last_angle):
+				_advance_crank(angle, _last_angle)
+			_last_angle = angle
+
+
+func _get_crank_angle() -> float:
+	if _using_mouse:
+		return (get_global_mouse_position() - global_position).angle()
+	var stick := Vector2(
+		Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
+		Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+	)
+	if stick.length() > 0.3:
+		return stick.angle()
+	return NAN
 
 
 func _physics_process(delta: float) -> void:
-    if !_is_launched:
-        return
-    velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-    move_and_slide()
-    if velocity == Vector2.ZERO:
-        _is_launched = false
-        _crank_degrees = 0.0
-        _last_full_rotations = 0
-
-
-func _handle_mouse_input() -> void:
-    if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-        var angle := (get_global_mouse_position() - global_position).angle()
-        if !_mouse_cranking:
-            _mouse_cranking = true
-            _mouse_last_angle = angle
-        else:
-            _advance_crank(angle, _mouse_last_angle)
-            _mouse_last_angle = angle
-    elif _mouse_cranking:
-        _mouse_cranking = false
-        _launch()
-
-
-func _handle_gamepad_input() -> void:
-    var trigger := Input.get_joy_axis(0, JOY_AXIS_TRIGGER_RIGHT)
-    var trigger_held := trigger > 0.5
-    var stick := Vector2(
-        Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
-        Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
-    )
-
-    if trigger_held:
-        if stick.length() > 0.3:
-            var angle := stick.angle()
-            if !_pad_cranking:
-                _pad_cranking = true
-                _pad_last_angle = angle
-            else:
-                _advance_crank(angle, _pad_last_angle)
-                _pad_last_angle = angle
-        else:
-            _pad_cranking = false
-    else:
-        _pad_cranking = false
-        if _pad_trigger_held:
-            _launch()
-    _pad_trigger_held = trigger_held
+	if !_is_launched:
+		return
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	move_and_slide()
+	if velocity == Vector2.ZERO:
+		_is_launched = false
+		_crank_degrees = 0.0
+		_last_full_rotations = 0
 
 
 func _advance_crank(new_angle: float, prev_angle: float) -> void:
-    var delta := wrapf(new_angle - prev_angle, -PI, PI)
-    if delta <= 0.0:
-        return
-    var to_add := minf(rad_to_deg(delta), MAX_CRANK_DEGREES - _crank_degrees)
-    if to_add <= 0.0:
-        return
-    _crank_degrees += to_add
-    _crank_sprite.rotation_degrees = _crank_degrees
-    var full := int(_crank_degrees / 360.0)
-    if full > _last_full_rotations:
-        _last_full_rotations = full
-        _audio_stream_player.play()
+	var delta := wrapf(new_angle - prev_angle, -PI, PI)
+	if delta <= 0.0:
+		return
+	var to_add := minf(rad_to_deg(delta), MAX_CRANK_DEGREES - _crank_degrees)
+	if to_add <= 0.0:
+		return
+	_crank_degrees += to_add
+	_crank_sprite.rotation_degrees = _crank_degrees
+	var full := int(_crank_degrees / 360.0)
+	if full > _last_full_rotations:
+		_last_full_rotations = full
+		_audio_stream_player.play()
 
 
 func _launch() -> void:
-    if _crank_degrees <= 0.0 || _is_launched:
-        return
-    _is_launched = true
-    var tween := create_tween()
-    tween.tween_property(_crank_sprite, "rotation_degrees", 0.0, RESET_CRANK_SECONDS).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-    velocity = - transform.y * (_crank_degrees / MAX_CRANK_DEGREES) * MAX_SPEED
+	if _crank_degrees <= 0.0 || _is_launched:
+		return
+	_is_launched = true
+	var tween := create_tween()
+	tween.tween_property(_crank_sprite, "rotation_degrees", 0.0, RESET_CRANK_SECONDS).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	velocity = - transform.y * (_crank_degrees / MAX_CRANK_DEGREES) * MAX_SPEED
