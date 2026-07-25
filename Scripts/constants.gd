b72eb7eb23b03690ad486cefc587b2f0ad455466
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-07-25 09:23:57 +0300

    cp

--- Scripts/constants.gd
+++ Scripts/constants.gd
@@ -1,9 +1,9 @@
 class_name Constants
 
 static var max_crank_degrees := 3.0 * 360.0
-static var max_speed := 1200.0
+static var max_speed := 1180.0
 static var friction := 600.0
-static var steer_speed := 160.0
+static var steer_speed := 200.0
 static var enemy_speed := 200.0
 static var enemy_kill_speed := 600.0
 static var gear_time_bonus := 3.0
@@ -11,7 +11,7 @@ static var enemy_kill_time_bonus := 3.0
 static var cannon_kill_time_bonus := 3.0
 static var rest_velocity_threshold := 300.0
 static var cannon_turret_speed_degrees := 90.0
-static var bullet_speed := 700.0
+static var bullet_speed := 400.0
 static var default_level_time := 15.0
 static var minion_respawn_delay := 2.0
 static var hazard_boss_spawn_interval := 3.0
