40c611a891bf5d13e8eb9428f406aa7941a6dfb4
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-07-25 19:21:38 +0300

    co

--- Scenes/Crank/crank.gd
+++ Scenes/Crank/crank.gd
@@ -6,62 +6,70 @@ signal crank_released(degrees: float)
 signal cycle_completed
 
 static var reset_crank_seconds := 1.0
+static var min_move_pixels := 3.0
 
 @onready var _crank_sprite: Sprite2D = $CrankSprite
 @onready var _progress_bar: TextureProgressBar = $ProgressBar
 @onready var _audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var _virtual_mouse_icon: Sprite2D = $VirtualMouseIcon
-@onready var _pivot_icon: Sprite2D = $PivotIcon
 
 var _crank_degrees: float = 0.0
 var _last_full_rotations: int = 0
 var _cranking: bool = false
 var _auto_cranking: bool = false
 var _last_angle: float = NAN
+var _last_move_angle: float = NAN
 var _using_mouse: bool = false
 var _virtual_mouse_pos: Vector2 = Vector2.ZERO
-var _crank_pivot: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
-    Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+    _virtual_mouse_pos = _crank_sprite.get_global_mouse_position()
+    _virtual_mouse_icon.global_position = _virtual_mouse_pos
     _progress_bar.min_value = 0.0
     _progress_bar.max_value = Constants.max_crank_degrees
     _progress_bar.value = 0.0
 
 func _input(event: InputEvent) -> void:
-    if !_cranking || !_using_mouse || Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
-        return
     if !(event is InputEventMouseMotion):
         return
     var relative := (event as InputEventMouseMotion).relative
     _virtual_mouse_pos += get_viewport().get_canvas_transform().affine_inverse().basis_xform(relative)
+    _clamp_virtual_mouse_pos()
     _virtual_mouse_icon.global_position = _virtual_mouse_pos
+    if !_cranking || !_using_mouse:
+        return
+    if relative.length() < min_move_pixels:
+        return
+    var move_angle := relative.angle()
+    if !is_nan(_last_move_angle):
+        _advance_crank(move_angle, _last_move_angle)
+    _last_move_angle = move_angle
+
+
+func _clamp_virtual_mouse_pos() -> void:
+    var transform := get_viewport().get_canvas_transform().affine_inverse()
+    var visible_rect := get_viewport().get_visible_rect()
+    var corner_a := transform * visible_rect.position
+    var corner_b := transform * visible_rect.end
+    _virtual_mouse_pos.x = clampf(_virtual_mouse_pos.x, minf(corner_a.x, corner_b.x), maxf(corner_a.x, corner_b.x))
+    _virtual_mouse_pos.y = clampf(_virtual_mouse_pos.y, minf(corner_a.y, corner_b.y), maxf(corner_a.y, corner_b.y))
 
 
 func _process(delta: float) -> void:
     if Input.is_action_just_pressed("crank"):
         _using_mouse = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
-        if _using_mouse:
-            _crank_pivot = _crank_sprite.get_global_mouse_position()
-            _virtual_mouse_pos = _crank_pivot
-            Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-            _virtual_mouse_icon.global_position = _virtual_mouse_pos
-            _virtual_mouse_icon.visible = true
-            _pivot_icon.global_position = _crank_pivot
-            _pivot_icon.visible = true
+        _last_move_angle = NAN
+        if !_using_mouse:
+            _last_angle = _get_crank_angle()
         _cranking = true
-        _last_angle = _get_crank_angle()
         crank_pressed.emit()
     elif Input.is_action_just_released("crank"):
-        if _using_mouse:
-            Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-            _virtual_mouse_icon.visible = false
-            _pivot_icon.visible = false
         _cranking = false
         var degrees_before_launch := _crank_degrees
         _try_launch()
         crank_released.emit(degrees_before_launch)
-    elif _cranking:
+    elif _cranking && !_using_mouse:
         var angle := _get_crank_angle()
         if is_nan(angle):
             _last_angle = NAN
@@ -88,8 +96,6 @@ func set_enabled(value: bool) -> void:
 
 
 func _get_crank_angle() -> float:
-    if _using_mouse:
-        return (_virtual_mouse_pos - _crank_pivot).angle()
     var stick := Vector2(
         Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
         Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
